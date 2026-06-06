146220ee0ff5c92e23c8611b8fecd4d8aff59ad2
PRIYANSHU RANJAN <78998038+priyanshu1428@users.noreply.github.com>
2026-06-06 21:00:24 +0000

    just like that

--- priyanshu.py
+++ priyanshu.py
@@ -1,2 +1,10 @@
-print("I AM LEARNING THIS THINGS SLOWLY")
+jls_extract_var = print
+jls_extract_var("I AM LEARNING THIS THINGS SLOWLY")
+print("SEE A MAGIC . enter a no.")
+a = int (input())
+b = a * 10
+c = a + 5 
+d = b + c 
+print("you are getting a ka multiple and a plus 5 ka sum "  )
+print (d)
 
